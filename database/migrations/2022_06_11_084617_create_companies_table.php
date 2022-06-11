533282e0c06d84c1a5d883c2bdb01b08430ee005
MartinPredanoczy <martin.predanoczy@gmail.com>
2022-06-11 16:41:03 +0200

    pridal som stlpce tabulky Companies

--- database/migrations/2022_06_11_084617_create_companies_table.php
+++ database/migrations/2022_06_11_084617_create_companies_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('adress');
+            $table->string('website');
             $table->timestamps();
         });
     }
