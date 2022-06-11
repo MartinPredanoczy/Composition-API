7fa4e383c5582192bfb8952975549ae4e0636bf1
MartinPredanoczy <martin.predanoczy@gmail.com>
2022-06-11 16:44:57 +0200

    uprava not null stlcov

--- database/migrations/2022_06_11_084617_create_companies_table.php
+++ database/migrations/2022_06_11_084617_create_companies_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
-            $table->string('adress');
-            $table->string('website');
+            $table->string('adress')->nullable();
+            $table->string('website')->nullable();
             $table->timestamps();
         });
     }
