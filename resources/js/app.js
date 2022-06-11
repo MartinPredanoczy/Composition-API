6b14cea9544d7f341cc9c8877fc79576d8b8360e
MartinPredanoczy <martin.predanoczy@gmail.com>
2022-06-11 19:19:54 +0200

    registracia komponentu CompaniesIndex

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,13 @@ window.Alpine = Alpine;
 Alpine.start();
 
 import { createApp } from 'vue';
+import router from "./components/router";
+import CompaniesIndex from './components/companies/CompaniesIndex.vue'
+
 const app = createApp({
-    components: {}
+    components: {
+        CompaniesIndex: 'companies-index',
+    }
 });
+app.use(router);
 app.mount('#app');
