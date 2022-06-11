ca12c218197c8cf4ff4e78a43ae93a93bcadd060
MartinPredanoczy <martin.predanoczy@gmail.com>
2022-06-11 19:30:13 +0200

    registracia komponentov vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,18 +2,21 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
-
-Alpine.start();
-
 import { createApp } from 'vue';
 import router from "./components/router";
 import CompaniesIndex from './components/companies/CompaniesIndex.vue'
 
 const app = createApp({
     components: {
-        CompaniesIndex: 'companies-index',
+        CompaniesIndex
     }
 });
 app.use(router);
 app.mount('#app');
+
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
