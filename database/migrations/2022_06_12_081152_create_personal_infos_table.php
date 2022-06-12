53954006190c2f5453789b0c943e520255fb190b
MartinPredanoczy <martin.predanoczy@gmail.com>
2022-06-12 10:31:20 +0200

    definovanie tabulky personal_infos

--- database/migrations/2022_06_12_081152_create_personal_infos_table.php
+++ database/migrations/2022_06_12_081152_create_personal_infos_table.php
@@ -15,7 +15,24 @@ return new class extends Migration
     {
         Schema::create('personal_infos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('title_before_name')->nullable();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('title_after_name')->nullable();
+            $table->string('birth_number');
+            $table->decimal('deposit', 20, 2);
+            $table->decimal('company_share', 5, 2);
+            $table->decimal('deposit_amount', 20, 2);
+            $table->char('sex', 1);
+            $table->string('nationality', 30);
+            $table->string('identity_document', 20);
+            $table->string('identity_document_number', 20);
+            $table->string('nationality', 30);
+            $table->boolean('is_partner');
+            $table->boolean('is_executive_manager');
+            $table->boolean('is_capital_administrator');
+            $table->boolean('is_residence_owner');
+            $table->boolean('is_ro_company');
         });
     }
 
